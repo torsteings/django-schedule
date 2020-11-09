14d78130805fc7f96bef41fb858d02529935f6b6
torsteings <torsteings>
2020-11-09 19:07:53 +0100

    Disabled STATIC_URL

--- jobs/settings.py
+++ jobs/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 EMAIL_HOST_USER = 'ctbirdsas@gmail.com'
 EMAIL_HOST_PASSWORD = "1stofApril"
