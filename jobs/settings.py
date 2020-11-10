1ff58b1eab2e04b65773e5cd48e3492c07e2c258
torsteings <torsteings>
2020-11-10 21:37:24 +0100

    Added App password

--- jobs/settings.py
+++ jobs/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_HOST_USER = 'ctbirdsas@gmail.com'
-EMAIL_HOST_PASSWORD = "1stofApril"
+EMAIL_HOST_PASSWORD = "ratzwfppycdpoukt"
 
 # import django_heroku
 # django_heroku.settings(locals())
