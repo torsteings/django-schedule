a6c452a47c2c41d0b74e29d46376bf8282b7801d
torsteings <torsteings>
2020-11-16 20:19:51 +0100

    Added request from API

--- scraping/management/commands/scrape.py
+++ scraping/management/commands/scrape.py
@@ -7,9 +7,59 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
+import numpy as np
+import pandas as pd
+
+import requests
+
 class Command(BaseCommand):
     # define logic of command
     def handle(self, *args, **options):
+        yahooAdress = "https://query1.finance.yahoo.com/v8/finance/chart/"
+        tickerSymbol = "EQNR" #ticker
+        symbol = ".OL?symbol="
+        rest = ".OL&period1=1533914279&period2=9999999999&interval=1d&includePrePost=true&events=div%2Csplit"
+        url = yahooAdress + tickerSymbol + symbol + tickerSymbol + rest
+        #url = "https://query1.finance.yahoo.com/v8/finance/chart/EQNR.OL?symbol=EQNR.OL&period1=1533914279&period2=9999999999&interval=1d&includePrePost=true&events=div%2Csplit"
+        r = requests.get(url)
+
+        #Checks if there is contact with the server: Status code: 200 = OK!
+        if r.status_code == 200:
+            #print("Status code: 200.....Standard response for successful HTTP requests")
+            data = r.json()
+            result = r.json()['chart']['result']
+
+            # Iterate through the list ['meta']. List does not contain {timestamp} dictionary.
+            # Append all items to metaList.
+            # {timestamp} is on the same level as ['meta'] and can be accessed through the for loop.
+            metaList = []
+            for item in result:
+                metaList.append(item['indicators']['quote'])
+            open = metaList[0][0]['open']
+            close = metaList[0][0]['close']
+            high = metaList[0][0]['high']
+            low = metaList[0][0]['low']
+            volume = metaList[0][0]['volume']
+
+            #open.reverse()
+            #close.reverse()
+            #high.reverse()
+            #low.reverse()
+            #volume.reverse()
+
+            dataMatrix = []
+            dataMatrix.append(open)
+            dataMatrix.append(close)
+            dataMatrix.append(high)
+            dataMatrix.append(low)
+            dataMatrix.append(volume)
+            #print(dataMatrix)
+
+            data = np.array(dataMatrix).T.tolist()
+            df = pd.DataFrame(data, index=range(0, len(open)), columns=['open_price', 'close_price', 'high_price', 'low_price', 'volume'])
+            close_price = df['close_price'].tail(1)
+            # return df['volume']
+            # return df
 
         # Set email header information.
         sender = "CTbirds AS"
@@ -20,7 +70,7 @@ class Command(BaseCommand):
 
         recipient = "torstein.skarsgard@gmail.com"
 
-        body = "var3"
+        body = "var3" + str(close_price)
 
         # Assemble the message.
         msg = MIMEMultipart()
@@ -41,5 +91,5 @@ class Command(BaseCommand):
         # Include a return statement at the end of each
         # logical flow of the handle() function so
         # Heroku Scheduler knows when it can shut down.
-        
+
         return
